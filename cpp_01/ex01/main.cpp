6ecf3f796674b891d01ec6121eaa0226d4ed0089
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2025-11-20 10:15:10 +0100

    ex01 : solution not done yet

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -4,10 +4,22 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
-	
+	Zombie *zombie = new Zombie[N];
+
+	int i = 0;
+	while(i < N)
+	{
+		zombie[i].setName(name);
+		i++;
+	}
+	return zombie;
 }
 
 int main()
 {
+	
+	Zombie* zombie = zombieHorde(3, "ayoub");
+	std::cout << zombie[2].getName() << std::endl;
+	delete[] zombie;
 
 }
