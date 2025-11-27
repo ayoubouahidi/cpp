e72dfedc6f460a37d127101faa7869630bab836a
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2025-11-27 10:14:05 +0000

    ex01 : done

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -4,10 +4,26 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
-	
+	Zombie* zombies = new Zombie[N];
+    int i = 0;
+
+    while (i < N)
+    {
+        zombies[i].setName(name);
+        i++;
+    }
+    return zombies;
 }
 
 int main()
 {
+    Zombie* zombies = zombieHorde(3, "ayoub");
+    int i = 0;
 
+    while (i < 3)
+    {
+        std::cout << zombies[i].getName() << std::endl;
+        i++;
+    }
+    delete[] zombies;
 }
