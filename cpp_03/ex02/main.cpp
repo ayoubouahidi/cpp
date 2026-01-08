ba02b8c9435fa49c4fbfa2efb114060ed58d699b
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2026-01-08 16:17:18 +0100

    some new tests done

--- cpp_03/ex02/main.cpp
+++ cpp_03/ex02/main.cpp
@@ -17,21 +17,6 @@ int main()
         trap.attack("Enemy");
         trap.beRepaired(2);
     }
-    // ScavTrap scrav("mook"); 
-    // ClapTrap Clap("babak");
-    // ScavTrap s("gg");
-    
-    // for (size_t i = 0; i < 60; i++)
-    // {
-    //     scrav.beRepaired(-1);
-
-    // }
-
-    // scrav.takeDamage(10);
-    // scrav.beRepaired(1);
-    // scrav = s;
-    // scrav.beRepaired(1);
-    // scrav.beRepaired(1);
 
     FragTrap frag("frag");
     FragTrap f("f");
